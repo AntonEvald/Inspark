5c6ccf7fa84007cdb5bb2335b5fc1fe99774a751
AndreasDaun <Andreasdaun@hotmail.com>
2018-03-06 13:49:33 +0100

    created pages

---
+++ Inspark/Inspark/Inspark/ContestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Inspark
+{
+    public partial class ContestPage : ContentPage
+    {
+        public ContestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
