facfea0903c0a49669ab9c422f95efdef820bd54
Andreas Dahlin <andreasdahlin@live.se>
2018-03-06 11:25:55 +0100

    Implemented XAML in RegisteredUserDetails

--- Inspark/Inspark/Inspark/RegisterUserDetails.xaml.cs
+++ Inspark/Inspark/Inspark/RegisterUserDetails.xaml.cs
@@ -11,5 +11,10 @@ namespace Inspark
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Register());
+        }
     }
 }
