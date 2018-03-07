0ddb359ce9e2870c2cabb9515b54e57b28a5068c
Anton Evald <anton.evald@gmail.com>
2018-03-07 13:14:05 +0100

    Model User

---
+++ Inspark/Inspark/Inspark/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspark.Models
+{
+    class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public byte ProfilePicture { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string Section { get; set; }
+        public bool KeepLoggedIn { get; set; }
+    }
+}
