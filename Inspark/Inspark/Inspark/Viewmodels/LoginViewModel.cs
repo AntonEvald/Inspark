7b306d06533e887baa11f55f77d344cebc3dccf0
Anton Evald <anton.evald@gmail.com>
2018-03-07 13:33:12 +0100

    .

---
+++ Inspark/Inspark/Inspark/Viewmodels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspark.Viewmodels
+{
+    class LoginViewModel
+    {
+        public bool Login_Clicked()
+        {
+            var enteredEmail = 
+        }
+    }
+}
