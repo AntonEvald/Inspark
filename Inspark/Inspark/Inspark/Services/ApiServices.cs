b55fae4803ccfd829af2bb74834c538be60720d2
andreasdaun <andreasdaun@hotmail.com>
2018-03-07 16:55:31 +0100

    created api class and finnished registerviewmodel

---
+++ Inspark/Inspark/Inspark/Services/ApiServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Inspark.Services
+{
+    public class ApiServices
+    {
+
+        public async Task<bool> RegisterAsync(string email,string password,string confirmpassword,string firstname,string lastname,int phonenumber)
+        {
+            //var client = new HttpClient();
+            var model = new Models.User
+            {
+                Email = email,
+                UserName = email,
+                Password = password,
+                FirstName = firstname,
+                LastName = lastname,
+                PhoneNumber = phonenumber
+
+            };
+
+            //var json = JsonConvert.SerializeObject(model);
+            //HttpContent content = new StringContent(json);
+            //var response = await client.PostAsync("url to webservice",content,);
+
+            return true; //response.isSuccessStatusCode;
+        }
+
+    }
+}
