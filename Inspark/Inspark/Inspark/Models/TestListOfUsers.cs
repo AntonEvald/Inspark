4a08e275807dded7dd7022d0afa107696c297728
Anton Evald <anton.evald@gmail.com>
2018-03-08 11:53:31 +0100

    MAX

---
+++ Inspark/Inspark/Inspark/Models/TestListOfUsers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspark.Models
+{
+    public class TestListOfUsers
+    {
+        public ICollection<User> Users { get; set; }
+    }
+}
--- Inspark/Inspark/Inspark/Viewmodels/LoginViewModel.cs
+++ Inspark/Inspark/Inspark/Viewmodels/LoginViewModel.cs
@@ -5,28 +5,99 @@ using System.Text;
 using System.ComponentModel;
 using Xamarin.Forms;
 using System.Windows.Input;
+using Inspark.Models;
+using System.Collections;
 
 namespace Inspark.Viewmodels
 {
-    public class LoginViewModel : ICommand
+    public class LoginViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
     {
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public bool KeepLoggedIn { get; set; }
+        private string _password;
+        public string Password {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if(_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                if (_Email != value)
+                {
+                    _Email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+
+        private bool keepLoggedIn;
+
+        public bool KeepLoggedIn
+        {
+            get { return keepLoggedIn; }
+            set
+            {
+                if (keepLoggedIn != value)
+                {
+                    keepLoggedIn = value;
+                    OnPropertyChanged("KeepLoggedIn");
+                }
+            }
+        }
+        public ICommand LoginClick { get; private set; }
+
+        public bool HasErrors => throw new NotImplementedException();
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public LoginViewModel()
         {
-
+            LoginClick = new Command (ValidateLogin);
         }
 
-        public event EventHandler CanExecuteChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
-        public bool CanExecute(object parameter)
+        void ValidateLogin()
         {
-            throw new NotImplementedException();
+            var listofusers = new List<User>();
+            var user = new User
+            {
+                Email = "anton@evald.se",
+                Password = "hej"
+            };
+            listofusers.Add(user);
+
+            foreach(var x in listofusers)
+            {
+                if(x.Email == Email && x.Password == Password)
+                {
+                    Debug.WriteLine("Funkar");
+                }
+                else
+                {
+                    Debug.WriteLine("Funkar");
+                }
+            }
         }
 
-        public void Execute(object parameter)
+        public IEnumerable GetErrors(string propertyName)
         {
             throw new NotImplementedException();
         }
