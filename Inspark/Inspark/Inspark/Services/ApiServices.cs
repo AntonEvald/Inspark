59ca85729370fa728b569d2dc4b6193bfadc43b8
andreasdaun <andreasdaun@hotmail.com>
2018-03-09 12:18:35 +0100

    .

--- Inspark/Inspark/Inspark/Services/ApiServices.cs
+++ Inspark/Inspark/Inspark/Services/ApiServices.cs
@@ -5,22 +5,34 @@ using Newtonsoft.Json;
 using Inspark.Models;
 using System.Text;
 using System.Collections.Generic;
+using System.IO;
 using System.Diagnostics;
 
 namespace Inspark.Services
 {
     public class ApiServices
     {
+        private byte[] pic;
 
         public async Task<bool> RegisterAsync(string role, string email,string password)
         {
+            
+#if __ANDROID__
+                Uri fileuri = Uri.parse("android.resource://drawable"+ R.drawable.DefaultPic);
+                var path = new File(fileuri.getPath());
+                pic = File.ReadAllBytes(path);
+
+#endif
+
+            
             var client = new HttpClient();
             var model = new User
             {
                 UserName = email,
                 Password = password,
                 Email = email,
-                Role = role
+                Role = role,
+                ProfilePicture = pic
                 //FirstName = firstname,
                 //LastName = lastname,
                 //PhoneNumber = phonenumber
