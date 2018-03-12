84c77027d70daa52779cdb6ecc28ef35adc417ad
Andreas Dahlin <andreasdahlin@live.se>
2018-03-12 11:31:16 +0100

    Ändrade metodnamn

--- Inspark/Inspark/Inspark/Views/MainPage.xaml.cs
+++ Inspark/Inspark/Inspark/Views/MainPage.xaml.cs
@@ -12,40 +12,39 @@ namespace Inspark.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
-        
         public MainPage()
         {
             InitializeComponent();
         }
 
-        public void Icon1_Tapped(object sender, EventArgs e)
+        public void HomeIconTapped(object sender, EventArgs e)
         {
             var page = new HomePage();
-            PlaceHolder.Content = page.Content;
+            Inspark.Content = page.Content;
         }
 
-        public void Icon2_Tapped(object sender, EventArgs e)
+        public void GroupIconTapped(object sender, EventArgs e)
         {
             var page = new GroupPage();
-            PlaceHolder.Content = page.Content;
+            Inspark.Content = page.Content;
         }
 
-        public void Icon3_Tapped(object sender, EventArgs e)
+        public void CompetitionIconTapped(object sender, EventArgs e)
         {
             var page = new CompetitionPage();
-            PlaceHolder.Content = page.Content;
+            Inspark.Content = page.Content;
         }
 
-        public void Icon4_Tapped(object sender, EventArgs e)
+        public void ChatIconTapped(object sender, EventArgs e)
         {
             var page = new ChatPage();
-            PlaceHolder.Content = page.Content;
+            Inspark.Content = page.Content;
         }
 
-        public void Icon5_Tapped(object sender, EventArgs e)
+        public void ScheduleIconTapped(object sender, EventArgs e)
         {
             var page = new SchedulePage();
-            PlaceHolder.Content = page.Content;
+            Inspark.Content = page.Content;
         }
     }
 }
