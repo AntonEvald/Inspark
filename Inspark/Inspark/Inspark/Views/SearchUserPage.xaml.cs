d3a9a154642784515fd642166ece7c112fac12ff
Andreas Dahlin <andreasdahlin@live.se>
2018-03-20 10:07:21 +0100

    SearchPage med en hardcodad sökfunktion som måste ändras /skrivet i codebehind, får inte viewmodel att fungera

---
+++ Inspark/Inspark/Inspark/Views/SearchUserPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inspark.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchUserPage : ContentPage
+	{
+        private readonly List<string> userList = new List<string>()
+        {
+            "Andreas","Philip","Anton","Jesper","Pedram","Patrik","Max"
+        };
+
+		public SearchUserPage()
+		{
+			InitializeComponent ();
+            UserList.ItemsSource = userList;
+		}
+        
+        public void SearchClicked(object sender, EventArgs e)
+        {
+            string keyword = SearchUser.Text;
+            IEnumerable<string> result = userList.Where(name => name.ToLower().Contains(keyword.ToLower()));
+            UserList.ItemsSource = result;
+        }
+
+
+    }
+}
