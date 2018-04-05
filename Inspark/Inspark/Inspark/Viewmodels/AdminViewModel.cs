00d35f6d7a4fccd38bb63cc2763314a4cd1f3187
Andreas Dahlin <andreasdahlin@live.se>
2018-04-05 11:10:23 +0200

    .

---
+++ Inspark/Inspark/Inspark/Viewmodels/AdminViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Inspark.Viewmodels
+{
+    public class AdminViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+
+    }
+}
