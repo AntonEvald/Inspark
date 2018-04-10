c66c37dbf6d3f5c9abbe39b08ab41dab4b9ab2cd
Andreas Dahlin <andreasdahlin@live.se>
2018-04-10 13:31:23 +0200

    BaseViewModel, INTE ändrat på alla ställen än

---
+++ Inspark/Inspark/Inspark/Viewmodels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Inspark.Viewmodels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnPropertyChangedTwo(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
