05104bd728223f816fd9f253734c24965213d963
Andreas Dahlin <andreasdahlin@live.se>
2018-05-02 12:52:55 +0200

    .

--- Inspark/Inspark/Inspark/Viewmodels/ChangeEventViewModel.cs
+++ Inspark/Inspark/Inspark/Viewmodels/ChangeEventViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inspark.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace Inspark.Viewmodels
 {
     public class ChangeEventViewModel
     {
-
+        ApiServices _api = new ApiServices();
     }
 }
