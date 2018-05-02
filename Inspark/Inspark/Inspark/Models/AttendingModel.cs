0c97106797fcf02e6ccbc02b362b9e42aeec347c
andreasdaun <andreasdaun@hotmail.com>
2018-05-02 11:08:10 +0200

    Added userId to settings

---
+++ Inspark/Inspark/Inspark/Models/AttendingModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Inspark.Models
+{
+    public class AttendingModel
+    {
+        public bool IsComing { get; set; }
+        public string UserId { get; set; }
+        public int GroupEventId { get; set; }
+    }
+}
