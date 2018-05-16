c0e5de8873d90d3f952b3c7e71bc946fb55acb17
Andreas Dahlin <andreasdahlin@live.se>
2018-05-16 11:31:17 +0200

    .

--- Inspark/Inspark/Inspark/Viewmodels/CompetitionViewModel.cs
+++ Inspark/Inspark/Inspark/Viewmodels/CompetitionViewModel.cs
@@ -54,6 +54,17 @@ namespace Inspark.Viewmodels
         public async void LoadResult()
         {
             var score = await _api.GetAllScore();
+            if (score.Count < 1)
+            {
+                var example = new Score
+                {
+                    Id = 1,
+                    TotalPoints = 100,
+                    GroupID = 2
+                };
+                score.Add(example);
+            }
+
             score = new ObservableCollection<Score>(score);
             Score = score;
         }
