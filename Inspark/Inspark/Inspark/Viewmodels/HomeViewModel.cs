5526496fc9a6ab75ab28e2d19b972632dd72f6da
mightykong <AndreasDaun@hotmail.com>
2018-05-17 10:42:40 +0200

    .

--- Inspark/Inspark/Inspark/Viewmodels/HomeViewModel.cs
+++ Inspark/Inspark/Inspark/Viewmodels/HomeViewModel.cs
@@ -11,137 +11,137 @@ using Xamarin.Forms;
 
 namespace Inspark.Viewmodels
 {
-    class HomeViewModel : BaseViewModel
-    {
-        private ObservableCollection<GroupPost> _groupPosts;
+	class HomeViewModel : BaseViewModel
+	{
+		private ObservableCollection<GroupPost> _groupPosts;
 
-        public ObservableCollection<GroupPost> GroupPosts
-        {
-            get { return  _groupPosts; }
-            set
-            {
-                if(_groupPosts != value)
-                {
-                    _groupPosts = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public ObservableCollection<GroupPost> GroupPosts
+		{
+			get { return _groupPosts; }
+			set
+			{
+				if (_groupPosts != value)
+				{
+					_groupPosts = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private ObservableCollection<NewsPost> _newsPosts;
+		private ObservableCollection<NewsPost> _newsPosts;
 
-        public ObservableCollection<NewsPost> NewsPosts
-        {
-            get { return _newsPosts; }
-            set
-            {
-                if(_newsPosts != value)
-                {
-                    _newsPosts = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public ObservableCollection<NewsPost> NewsPosts
+		{
+			get { return _newsPosts; }
+			set
+			{
+				if (_newsPosts != value)
+				{
+					_newsPosts = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private bool _newsIsRefreshing;
+		private bool _newsIsRefreshing;
 
-        public bool NewsIsRefreshing
-        {
-            get { return _newsIsRefreshing; }
-            set
-            {
-                if(_newsIsRefreshing != value)
-                {
-                    _newsIsRefreshing = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public bool NewsIsRefreshing
+		{
+			get { return _newsIsRefreshing; }
+			set
+			{
+				if (_newsIsRefreshing != value)
+				{
+					_newsIsRefreshing = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private bool _groupIsRefreshing;
+		private bool _groupIsRefreshing;
 
-        public bool GroupIsRefreshing
-        {
-            get { return _groupIsRefreshing; }
-            set
-            {
-                if (_groupIsRefreshing != value)
-                {
-                    _groupIsRefreshing = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public bool GroupIsRefreshing
+		{
+			get { return _groupIsRefreshing; }
+			set
+			{
+				if (_groupIsRefreshing != value)
+				{
+					_groupIsRefreshing = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
 
-        public ICommand RefreshNewsCommand
-        {
-            get
-            {
-                return new Command(() =>
-                {
-                    RefreshNewsListView();
-                });
-            }
-        }
+		public ICommand RefreshNewsCommand
+		{
+			get
+			{
+				return new Command(() =>
+				{
+					RefreshNewsListView();
+				});
+			}
+		}
 
-        public ICommand RefershGroupCommand
-        {
-            get
-            {
-                return new Command(() =>
-                {
-                    RefreshGroupListView();
-                });
-            }
-        }
+		public ICommand RefershGroupCommand
+		{
+			get
+			{
+				return new Command(() =>
+				{
+					RefreshGroupListView();
+				});
+			}
+		}
 
-        private async void RefreshNewsListView()
-        {
-            NewsIsRefreshing = true;
-            var posts = await _api.GetAllNewsPosts();
-            if (posts.Count < 1)
-            {
-                var post = new NewsPost()
-                {
-                    Author = "Admin",
-                    Text = "Det finns inga poster ännu.",
-                    Title = "Det finns inga poster ännu.",
-                    Date = DateTime.Now,
-                    Picture = null,
-                };
-                posts.Add(post);
-            }
-            posts = new ObservableCollection<NewsPost>(posts.OrderByDescending(i => i.Date).OrderByDescending(i => i.Pinned));
-            NewsPosts = posts;
-            NewsIsRefreshing = false;
-        }
+		private async void RefreshNewsListView()
+		{
+			NewsIsRefreshing = true;
+			var posts = await _api.GetAllNewsPosts();
+			if (posts.Count < 1)
+			{
+				var post = new NewsPost()
+				{
+					Author = "Admin",
+					Text = "Det finns inga poster ännu.",
+					Title = "Det finns inga poster ännu.",
+					Date = DateTime.Now,
+					Picture = null,
+				};
+				posts.Add(post);
+			}
+			posts = new ObservableCollection<NewsPost>(posts.OrderByDescending(i => i.Date).OrderByDescending(i => i.Pinned));
+			NewsPosts = posts;
+			NewsIsRefreshing = false;
+		}
 
-        private async void RefreshGroupListView()
-        {
-            GroupIsRefreshing = true;
-            var posts = await _api.GetAllGroupPosts();
-            posts = new ObservableCollection<GroupPost>(posts.OrderByDescending(i => i.Date));
-            if (posts.Count < 1)
-            {
-                var post = new GroupPost()
-                {
-                    Author = "Admin",
-                    Text = "Det finns inga poster ännu.",
-                    Title = "Det finns inga poster ännu.",
-                    Date = DateTime.Now,
-                    Picture = null
-                };
-                posts.Add(post);
-            }
-            GroupPosts = posts;
-            GroupIsRefreshing = false;
-        }
+		private async void RefreshGroupListView()
+		{
+			GroupIsRefreshing = true;
+			var posts = await _api.GetAllGroupPosts();
+			posts = new ObservableCollection<GroupPost>(posts.OrderByDescending(i => i.Date));
+			if (posts.Count < 1)
+			{
+				var post = new GroupPost()
+				{
+					Author = "Admin",
+					Text = "Det finns inga poster ännu.",
+					Title = "Det finns inga poster ännu.",
+					Date = DateTime.Now,
+					Picture = null
+				};
+				posts.Add(post);
+			}
+			GroupPosts = posts;
+			GroupIsRefreshing = false;
+		}
 
-        public HomeViewModel()
-        {
-            RefreshNewsListView();
-            RefreshGroupListView();
-        }
-    }
+		public HomeViewModel()
+		{
+			RefreshNewsListView();
+			RefreshGroupListView();
+		}
+	}
 }
--- Inspark/Inspark/Inspark/Viewmodels/ScheduleViewModel.cs
+++ Inspark/Inspark/Inspark/Viewmodels/ScheduleViewModel.cs
@@ -11,126 +11,126 @@ using System.Linq;
 
 namespace Inspark.Viewmodels
 {
-    public class ScheduleViewModel : BaseViewModel
-    {
-        
+	public class ScheduleViewModel : BaseViewModel
+	{
+
 		private ObservableCollection<CombindEvent> _specificDates;
 
 		public ObservableCollection<CombindEvent> SpecificDates
-        {
-            get { return _specificDates; }
-            set
-            {
-                if (_specificDates != value)
-                {
-                    _specificDates = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		{
+			get { return _specificDates; }
+			set
+			{
+				if (_specificDates != value)
+				{
+					_specificDates = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private ObservableCollection<GroupEvent> _groupsEvent;
+		private ObservableCollection<GroupEvent> _groupsEvent;
 
-        public ObservableCollection<GroupEvent> GroupEvent
-        {
-            get { return _groupsEvent; }
-            set
-            {
-                if (_groupsEvent != value)
-                {
-                    _groupsEvent = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public ObservableCollection<GroupEvent> GroupEvent
+		{
+			get { return _groupsEvent; }
+			set
+			{
+				if (_groupsEvent != value)
+				{
+					_groupsEvent = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private ObservableCollection<Event> _event;
+		private ObservableCollection<Event> _event;
 
-        public ObservableCollection<Event> Event
-        {
-            get { return _event; }
-            set
-            {
-                if (_event != value)
-                {
-                    _event = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public ObservableCollection<Event> Event
+		{
+			get { return _event; }
+			set
+			{
+				if (_event != value)
+				{
+					_event = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
 		private ObservableCollection<SpecialDate> _events;
 
 		public ObservableCollection<SpecialDate> Events
-        {
-            get { return _events; }
-            set
-            {
-                if (_events != value)
-                {
-                    _events = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		{
+			get { return _events; }
+			set
+			{
+				if (_events != value)
+				{
+					_events = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        private DateTime _date;
+		private DateTime _date;
 
-        public DateTime Date
-        {
-            get
-            {
-                return _date;
-            }
-            set
-            {
-                _date = value;
-                OnPropertyChanged();
-            }
-        }
+		public DateTime Date
+		{
+			get
+			{
+				return _date;
+			}
+			set
+			{
+				_date = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public async void populateLists()
-        {
-            var events = await _api.GetAllEventsAttending();
-            Event = events;
-            var groupEvents = await _api.GetAllGroupEventsAttending();
-            GroupEvent = groupEvents;
+		public async void populateLists()
+		{
+			var events = await _api.GetAllEventsAttending();
+			Event = events;
+			var groupEvents = await _api.GetAllGroupEventsAttending();
+			GroupEvent = groupEvents;
 
 			if (Event.Count() > 0)
 			{
-                            foreach (var item in Event)
-            {
-                var specialDate = new SpecialDate(item.TimeForEvent);
-                specialDate.BackgroundColor = Color.Green;
-                specialDate.TextColor = Color.White;
-                specialDate.Selectable = true;
-                Events.Add(specialDate);
-            }
+				foreach (var item in Event)
+				{
+					var specialDate = new SpecialDate(item.TimeForEvent);
+					specialDate.BackgroundColor = Color.Green;
+					specialDate.TextColor = Color.White;
+					specialDate.Selectable = true;
+					Events.Add(specialDate);
+				}
 			}
-            
+
 			if (GroupEvent.Count() > 0)
 			{
 				foreach (var item in GroupEvent)
-                {
-                    var specialDate = new SpecialDate(item.TimeForEvent);
-                    specialDate.BackgroundColor = Color.Green;
-                    specialDate.TextColor = Color.White;
-                    specialDate.Selectable = true;
-                    Events.Add(specialDate);
-                }
+				{
+					var specialDate = new SpecialDate(item.TimeForEvent);
+					specialDate.BackgroundColor = Color.Green;
+					specialDate.TextColor = Color.White;
+					specialDate.Selectable = true;
+					Events.Add(specialDate);
+				}
 			}
 
 
 		}
 
 		public ScheduleViewModel()
-        {
+		{
 			Events = new ObservableCollection<SpecialDate>();
 			SpecificDates = new ObservableCollection<CombindEvent>();
 			Event = new ObservableCollection<Event>();
 			GroupEvent = new ObservableCollection<GroupEvent>();
-            populateLists();
-        }
+			populateLists();
+		}
 
 
 		public ICommand DateChosen => new Command((obj) =>
@@ -141,7 +141,7 @@ namespace Inspark.Viewmodels
 			{
 				if (item.TimeForEvent.ToString("yyyy/MM/dd") == Date.ToString("yyyy/MM/dd"))
 				{
-					 var model = new CombindEvent
+					var model = new CombindEvent
 					{
 						Title = item.Title,
 						TimeForEvent = item.TimeForEvent,
@@ -152,19 +152,19 @@ namespace Inspark.Viewmodels
 				}
 			}
 			foreach (var item in GroupEvent)
-            {
-                if (item.TimeForEvent.ToString("yyyy/MM/dd") == Date.ToString("yyyy/MM/dd"))
-                {
-                    var model = new CombindEvent
-                    {
-                        Title = item.Title,
-                        TimeForEvent = item.TimeForEvent,
-                        Id = item.Id,
+			{
+				if (item.TimeForEvent.ToString("yyyy/MM/dd") == Date.ToString("yyyy/MM/dd"))
+				{
+					var model = new CombindEvent
+					{
+						Title = item.Title,
+						TimeForEvent = item.TimeForEvent,
+						Id = item.Id,
 						Type = "GroupEvent"
-                    };
-                    SpecificDates.Add(model);
-                }
-            }
+					};
+					SpecificDates.Add(model);
+				}
+			}
 
 		});
 
